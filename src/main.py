9e8fb6fb1ba4d2218712e156f9b3f68381bd3b4d
ricaym <aymerincchassagne@gmail.com>
2026-06-24 13:02:44 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.model import SentimentModel
 app = FastAPI(title="SentimentAI", version=" 0.1.0 ")
 
 model = SentimentModel()
-# Commentaire de test
+# Commentaire test
 
 
 @app.get("/health")

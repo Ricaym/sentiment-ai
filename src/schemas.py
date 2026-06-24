e559453e227d43d0f356707efbfdb1a6ea4262df
ricaym <aymerincchassagne@gmail.com>
2026-06-24 12:40:37 +0200

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field
 from typing import Literal
 
 class PredictionRequest(BaseModel):
-	text: str=Field(... , min_length=1 , max_length=5000)
+    text: str=Field(... , min_length=1 , max_length=5000)
 
 class PredictionResponse(BaseModel):
-	label: Literal["POSITIVE", "NEGATIVE", "NEUTRAL"]
-	score: float
-	text: str
+    label: Literal["POSITIVE", "NEGATIVE", "NEUTRAL"]
+    score: float
+    text: str
